2c83735031d138c77f8e7b9a9935929a6c573b7d
vicentsasi <96072949+vicentsasi@users.noreply.github.com>
2023-11-23 18:33:43 +0100

    - Extractores

---
+++ CATextractor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+public class CATextractor
+{
+    public static void LoadJsonDataIntoDatabase(string jsonFilePath)
+    {
+        string jsonData = File.ReadAllText(jsonFilePath);
+
+        // Deserializar JSON a una lista de objetos dinámicos
+        List<dynamic> dynamicDataList = JsonConvert.DeserializeObject<List<dynamic>>(jsonData);
+
+        foreach (var dynamicData in dynamicDataList)
+        {
+            // Extraer propiedades específicas y construir las columnas que deseas
+            string tipoVia = dynamicData.Tipo_via;
+            string direccion = dynamicData.Direccion;
+            string numero = dynamicData.Numero;
+
+            // Concatenar los valores en una sola cadena para la columna "Direccion"
+            string direccionCompleta = $"{tipoVia} {direccion} {numero}";
+
+            // Extraer el código postal para la columna "CodigoPostal"
+            string codigoPostal = dynamicData.Codigo_postal;
+
+            // Luego, puedes usar estas columnas para insertar en la base de datos
+            InsertIntoDatabase(direccionCompleta, codigoPostal);
+        }
+    }
+
+    private static void InsertIntoDatabase(string direccionCompleta, string codigoPostal)
+    {
+        // Aquí implementa la lógica para insertar las columnas en la base de datos
+        // Puedes utilizar Entity Framework u otro método según tu elección
+        // y la estructura de tu base de datos
+        // Ejemplo: using (var context = new ApplicationDbContext()) { ... }
+        Console.WriteLine($"Insertando en la base de datos: Direccion={direccionCompleta}, CodigoPostal={codigoPostal}");
+    }
+
+    // Ejemplo de uso
+    static void Main()
+    {
+        string jsonFilePath = "archivo.json";
+        LoadJsonDataIntoDatabase(jsonFilePath);
+    }
+}
+
