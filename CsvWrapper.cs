4a15a858d8cb2ed8293f3ceba5984cc45f8c4e95
vicentsasi <96072949+vicentsasi@users.noreply.github.com>
2023-11-23 18:14:18 +0100

    - Wrappers

---
+++ CsvWrapper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Newtonsoft.Json;
+
+public class CsvWrapper
+{
+    public static string ConvertToJson(string csvFilePath)
+    {
+        var records = new List<Dictionary<string, object>>();
+
+        using (var reader = new StreamReader(csvFilePath))
+        using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)))
+        {
+            var recordsFromCsv = csv.GetRecords<dynamic>();
+
+            foreach (var record in recordsFromCsv)
+            {
+                var dictionary = new Dictionary<string, object>();
+                foreach (var property in record.GetType().GetProperties())
+                {
+                    dictionary[property.Name] = property.GetValue(record);
+                }
+                records.Add(dictionary);
+            }
+        }
+
+        string json = JsonConvert.SerializeObject(records);
+        return json;
+    }
+
+    // Ejemplo de uso
+    static void Main()
+    {
+        string csvFilePath = "archivo.csv";
+        string jsonData = ConvertToJson(csvFilePath);
+        Console.WriteLine(jsonData);
+    }
+}
+
+
---
+++ XmlWrapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web;
+using System.Xml;
+using Newtonsoft.Json;
+
+public class XmlWrapper
+{
+
+    public static string ConvertToJson(string xmlFilePath)
+    {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.Load(xmlFilePath);
+
+        string json = JsonConvert.SerializeXmlNode(xmlDoc);
+        return json;
+    }
+
+    // Ejemplo de uso
+    static void Main()
+    {
+        string xmlFilePath = "archivo.xml";
+        string jsonData = ConvertToJson(xmlFilePath);
+        Console.WriteLine(jsonData);
+    }
+}
+
