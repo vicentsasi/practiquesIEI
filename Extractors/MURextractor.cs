656e3fe10bb04ec653dd130d76b05cf3b1834b64
Sergi Barberà Barberà <SBARBAR@upv.edu.es>
2023-11-25 19:34:36 +0100

    asda

--- Extractors/MURextractor.cs
+++ Extractors/MURextractor.cs
@@ -7,65 +7,74 @@ using practiquesIEI.Entities;
 namespace practiquesIEI.Extractors
 {
     public class MURextractor
-    {
-
-        
+    {        
         public static void LoadJsonDataIntoDatabase(string jsonFilePath)
         {
-            string jsonData = jsonFilePath;
+            try {
 
-            // Deserializar JSON a una lista de objetos dinámicos
-            List<dynamic> dynamicDataList = JsonConvert.DeserializeObject<List<dynamic>>(jsonData);
+                // Deserializar JSON a una lista de objetos dinámicos
+                List<dynamic> dynamicDataList = JsonConvert.DeserializeObject<List<dynamic>>(jsonFilePath);
 
-            foreach (var dynamicData in dynamicDataList)
-            {
-                
-                localidad local = new localidad();
-                provincia provincia = new provincia();
-
-                //Creamos y inicializamos el centro
-                centro_educativo centro = JsonACentro(dynamicData);
-
-                //Creamos la provincia 
-                provincia.codigo = 30;
-                provincia.nombre = "Múrcia";
-
-
-                local.nombre = dynamicData.muncen;
-                local.codigo = 1234;
-
-                /*
-                 * Insertar centro en la base de datos
-                 * centro_educativo centroPr = new centro_educativo(datos que traus del JSON);
-                 * ConexionBD.insertCentro(centroPr);
-                */
-                
+                foreach (var dynamicData in dynamicDataList)
+                {
+                    //Crear el centro
+                    centro_educativo centro = JsonACentro(dynamicData);
+                    //Crear la provincia 
+                    provincia provincia = new provincia
+                    {
+                        codigo = 30,
+                        nombre = "Múrcia"
+                    };
+                    //Crear localidad
+                    localidad localidad = new localidad
+                    {
+                        nombre = dynamicData.muncen,
+                        codigo = 1234
+                    };
+                    //Insertar centro en la base de datos
+                    ConexionBD.insertCentro(centro);
+                }
+            } 
+            catch (Exception e) {
+                Console.WriteLine($"Error: {e.Message}");
             }
+            
         }
 
         public static centro_educativo JsonACentro(dynamic dynamicData) {
-            centro_educativo centro = new centro_educativo();
-            
-            centro.nombre = dynamicData.dencen;
-            centro.direccion = dynamicData.domcen;
-            centro.telefono = dynamicData.telcen;
-            centro.descripcion = dynamicData.presentacionCorta;
-            centro.cod_postal = dynamicData.cpcen;
+            try {
+                centro_educativo centro = new centro_educativo
+                {
+                    nombre = dynamicData.denCorta + dynamicData.dencen,
+                    direccion = dynamicData.domcen,
+                    telefono = dynamicData.telcen,
+                    descripcion = dynamicData.presentacionCorta,
+                    cod_postal = dynamicData.cpcen,
+                    longitud = dynamicData.lon,
+                    latitud = dynamicData.lat,
+                };
+                switch (dynamicData.titularidad)
+                {
+                    case "P":
+                        centro.tipo = tipo_centro.Público;
+                        break;
+                    case "N":
+                        centro.tipo = tipo_centro.Privado;
+                        break;
+                    case "C":
+                        centro.tipo = tipo_centro.Concertado;
+                        break;
+                }
 
-            switch (dynamicData.titularidad) {
-                case "P": centro.tipo = tipo_centro.Público;
-                    break;
-                case "N": centro.tipo = tipo_centro.Privado;
-                    break;
-                case "C": centro.tipo = tipo_centro.Concertado;
-                    break;
+                return centro;
             }
-
-            return centro;
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+                return null;
+            }
+            
         }
-
-        
-
     }
 }
 
--- main.cs
+++ main.cs
@@ -18,13 +18,11 @@ namespace practiquesIEI
         /// </summary>
         [STAThread]
         static async Task Main()
-        {
-            //await ConexionBD.Conectar();
-            string file = "C:\\Users\\Usuario\\source\\repos\\practiquesIEI\\Fuentes de datos\\murcia.json";
+        {   
+            await ConexionBD.Conectar();
+            string file = "C:\\Users\\Sergi\\Source\\Repos\\vicentsasi\\practiquesIEI\\Fuentes de datos\\murcia.json";
             string arxivo = JsonWrapper.ConvertToJson(file);
             MURextractor.LoadJsonDataIntoDatabase(arxivo);
-            //centro_educativo centroPr = new centro_educativo("sant francesc de borja", tipo_centro.Público, 2023049, 2342834, 678363453, "es una escola de llombai molt re asea", "C/Avinguda Pais Valencia", 46195);
-            //ConexionBD.insertCentro(centroPr);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
