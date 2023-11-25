3594fe0f40b4e837666e96c0b5617944634b468f
Sergi Barberà Barberà <SBARBAR@upv.edu.es>
2023-11-25 13:29:13 +0100

    ordern

--- Extractors/CVextractor.cs
+++ Extractors/CVextractor.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
 
-public class DatabaseLoader
+public class CVextractor
 {
     public static void LoadJsonDataIntoDatabase(string jsonFilePath)
     {
