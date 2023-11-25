d7774baed3af331d25699c9f29a3f183f58ccdbc
Sergi Barberà Barberà <SBARBAR@upv.edu.es>
2023-11-25 15:13:18 +0100

    conn

--- conexionBD.cs
+++ conexionBD.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
+using practiquesIEI.Entities;
 
 namespace practiquesIEI
 {
-    public class conexionBD
+    public class ConexionBD
     {
         private static MySqlConnection conn;
-        public static async void Conectar()
+        public static async Task Conectar()
         {
             Console.WriteLine("Conectando");
             if (conn != null)
@@ -19,14 +23,38 @@ namespace practiquesIEI
             }
             try
             {
-                string connectionString = "server=172.23.186.115;port=3306;user=Administrador;password=root;database=centrosbd;";
+                string connectionString = "" +
+                    "server=172.23.186.115;" +
+                    "port=3306;" +
+                    "user=Administrador;" +
+                    "password=root;" +
+                    "database=centrosbd;";
                 conn = new MySqlConnection(connectionString);
                 await conn.OpenAsync();
                 Console.WriteLine("Conectado");
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.ToString() + "Error");
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+        public static async void insertCentro(centro_educativo centro) {
+            try
+            {
+                if (conn == null)
+                {
+                   await Conectar();
+                }
+                using (MySqlCommand command = new MySqlCommand(
+                  $"INSERT INTO centro_educativo (nombre, tipo, direccion, codigo_postal, longitud, latitud, telefono, descripcion) VALUES ('{centro.nombre}', '{centro.tipo}', '{centro.direccion}', '{centro.cod_postal}', '{centro.longitud}', '{centro.latitud}', '{centro.telefono}','{centro.descripcion}')", conn))
+                {
+                    await command.ExecuteNonQueryAsync();
+                    Console.WriteLine("Datos insertados correctamente.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error al ejecutar el comando: {e.Message}");
             }
         }
     }
--- main.cs
+++ main.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using practiquesIEI.Entities;
+using practiquesIEI;
+using System.Diagnostics;
+
+
+
 
 namespace practiquesIEI
 {
@@ -12,9 +18,11 @@ namespace practiquesIEI
         /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
-        static void Main()
+        static async Task Main()
         {
-            conexionBD.Conectar();
+            await ConexionBD.Conectar();
+            //centro_educativo centroPr = new centro_educativo("sant francesc de borja", tipo_centro.Público, 2023049, 2342834, 678363453, "es una escola de llombai molt re asea", "C/Avinguda Pais Valencia", 46195);
+            //ConexionBD.insertCentro(centroPr);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
