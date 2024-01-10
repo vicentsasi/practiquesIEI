88a82a04cff8a1257b58c514c655ffb77dcb50bd
Sergi Barberà Barberà <SBARBAR@upv.edu.es>
2024-01-10 13:28:29 +0100

    asd

--- Búsqueda.cs
+++ Búsqueda.cs
@@ -200,7 +200,7 @@ namespace practiquesIEI
 
                                     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                                         attribution: 'Map data &copy; <a href=""http://openstreetmap.org"">OpenStreetMap</a> contributors, <a href=""http://creativecommons.org/licenses/by-sa/2.0/"">CC-BY-SA</a>, Imagery © <a href=""http://cloudmade.com"">CloudMade</a>',
-                                        maxZoom: 13
+                                        maxZoom: 25
                                     }).addTo(map);
 
                                     L.control.scale().addTo(map);
